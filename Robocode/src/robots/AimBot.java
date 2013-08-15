3606b4402bba5633fe5baf2b8b1f75d454bf5999
Erick Lucena <elps@cin.ufpe.br>
2013-08-15 00:32:37 -0300

    Melhoria em "AimBot". No caso de mútiplos inimigos, mantém a mira no que foi visto primeiro até que o mesmo seja destruído. Após isso, procura um novo robô para ser seu alvo.

--- Robocode/src/robots/AimBot.java
+++ Robocode/src/robots/AimBot.java
@@ -8,25 +8,24 @@ import robocode.util.Utils;
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 /**
- * Kamikaze - a robot by (your name here)
+ * AimBot
  */
 public class AimBot extends AdvancedRobot
 {
-	/**
-	 * run: Kamikaze's default behavior
-	 */
+	
+	private String target = null;
+	
 	public void run() {
-	    // ...
 	 
 	    do {
-	        // ...
+
 	        // Turn the radar if we have no more turn, starts it if it stops and at the start of round
 	        if ( getRadarTurnRemaining() == 0.0 )
 	            setTurnRadarRightRadians( Double.POSITIVE_INFINITY );
 	        execute();
 	    } while ( true );
 	 
-	    // ...
+
 	}
 	 
 	public void onScannedRobot(ScannedRobotEvent e) {
@@ -34,16 +33,33 @@ public class AimBot extends AdvancedRobot
 		/**
 		 * Módulo de scan perfeito para um robô.
 		 */
-	    double angleToEnemy = getHeadingRadians() + e.getBearingRadians();	 
-	    double radarTurn = Utils.normalRelativeAngle( angleToEnemy - getRadarHeadingRadians() );
-	    double gunTurn = Utils.normalRelativeAngle( angleToEnemy - getGunHeadingRadians() );
-	    double extraTurn = Math.min( Math.atan( 36.0 / e.getDistance() ), Rules.RADAR_TURN_RATE_RADIANS );
-	 
-	    radarTurn += (radarTurn < 0 ? -extraTurn : extraTurn);
-	 
-	    setTurnRadarRightRadians(radarTurn);
-	    setTurnGunRightRadians(gunTurn);
-	    
+		
+		//Caso não haja nenhum robô como alvo...
+		if(target == null){
+			target = e.getName();
+			System.out.println("Target acquired!");
+		}
+		
+		if(e.getName().equals(target)){
+		    double angleToEnemy = getHeadingRadians() + e.getBearingRadians();	 
+		    double radarTurn = Utils.normalRelativeAngle( angleToEnemy - getRadarHeadingRadians() );
+		    double gunTurn = Utils.normalRelativeAngle( angleToEnemy - getGunHeadingRadians() );
+		    double extraTurn = Math.min( Math.atan( 36.0 / e.getDistance() ), Rules.RADAR_TURN_RATE_RADIANS );
+		 
+		    radarTurn += (radarTurn < 0 ? -extraTurn : extraTurn);
+		 
+		    setTurnRadarRightRadians(radarTurn);
+		    setTurnGunRightRadians(gunTurn);
+		}
+	}
+	
+	
+	//Quando um robô é destruido, verifica se é o atual alvo.
+	public void onRobotDeath(RobotDeathEvent e){
+		if(e.getName().equals(target)){
+			System.out.println("Yippee ki-yay, motherfucker!");
+			target = null;
+		}
 	}
 
 	
